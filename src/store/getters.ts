0a86cbccde731c6ab39f3c61720e60c1082f23ad
vsaraev <work.saraev@gmail.com>
2022-08-13 08:51:06 +0300

    Add getters

---
+++ src/store/getters.ts
@@ -0,0 +1,20 @@
+import { formatter } from "../operations/formatter";
+import { GetterTree } from "vuex";
+import { FormattedSunTime, FormattedTemperature, WidgetState } from "./types";
+
+export const getters: GetterTree<WidgetState, WidgetState> = {
+	sunTime(state) {
+		return {
+            sunset: formatter.formatUnixTime(state.sys.sunset),
+            sunrise: formatter.formatUnixTime(state.sys.sunrise),
+        } as FormattedSunTime
+	},
+    temperature(state) {
+        return {
+            temp: formatter.formatTemperature(state.main.temp),
+            feels_like: formatter.formatTemperature(state.main.feels_like),
+            temp_min: formatter.formatTemperature(state.main.temp_min),
+            temp_max: formatter.formatTemperature(state.main.temp_max)
+        } as FormattedTemperature
+    }
+}
